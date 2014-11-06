1312ec1ff73f2293101505712f5b1d09bbdc7f09
carlsmith93 <cesmith@fullsail.edu>
2014-11-06 19:01:01 -0400

    Final changes to the project.  These changes include functions as well adding in the string function and fixing the story in the html.

--- Smith_Carl_Assignment2.js
+++ Smith_Carl_Assignment2.js
@@ -7,9 +7,9 @@ Javascript flowchart program
 
 //Variables
 var dead = "Jingo has 0 HP left and has died." //String Variable
-	int = 13 //Number Variable
-	str = 10 //Number Variable
+	gold = 0 //Number Variable
 	congrats = "Congratulations on your victory!"
+	var Jingo = "While adventuring you find Jingo, a notorious assassin from the land of Draco Falls! Do you need to think of a strategy before you attack him? Click OK for yes."
 	
 
 //prompt
@@ -36,15 +36,16 @@ var PrimaryElement = prompt("What is your primary element? Enter fire or earth")
 var boolean = confirm("Are you ready to go on your first adventure and look for a battle? Click OK for yes.")
 
 //Boolean Function
-var Jingo = "While adventuring you find Jingo, a notorious assassin from the land of Draco Falls! Do you need to think of a strategy before you attack him? Click OK for yes."
 
-var prepare = confirm(Jingo)
+var prepare = function(prepare) {
+prepare(Jingo)
 	if (prepare == true){
 		var ready = true
 	}
 	else {
 		var ready = false
 	}
+}
 
 var prepandattacktime = 10
 
@@ -56,8 +57,28 @@ while (prepandattacktime > 0){
 }
 console.log("You have finished preparing and are now ready for the attack!")
 
-//Number Function
-var maxdamage = int + str //Maximum damage that can be dealt in a single attack
 
-for (var enemyhp = 150; enemyhp > 0; enemyhp -= maxdamage) {console.log("You raise your "+PrimaryWeapon+" and smash it into Jingo!  Jingo has "+enemyhp+" remaining!") };
+//Number Function
+function getdamage(int, str){
+	var int = 13
+	var str = 10
+	var getdamage = int+str
+
+
+}
+int = 13
+str = 10
+var damagedealt = int + str
+for(var enemyhp = 150; enemyhp > 0; enemyhp -= damagedealt) {console.log("You raise your "+PrimaryWeapon+" and smash it into Jingo!  Jingo has "+enemyhp+" hp remaining!") };
 console.log(dead)
+
+//String Function
+var ending1 = confirm("Jingo is now dead")
+var ending2 = confirm("therefore your adventure for today is now complete!")
+var reward = "You now loot your reward which is "
+gold +=1000
+console.log(ending1 + ending2 +reward + gold+" gp!")
+
+//final output
+
+console.log("in just today alone "+name+" has proven themself to be a very competant"+heroclass,"."+"during your fight you dealt a wopping 23 damage per hit for a total of 161 damage!  After the death of Jingo, his body was looted to give you 1000 gold for your future upgrades!"+congrats)
