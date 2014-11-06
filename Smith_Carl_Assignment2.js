50bd500b08e840ffadab18873ce1444635b5c2b4
carlsmith93 <cesmith@fullsail.edu>
2014-11-06 16:10:51 -0400

    Added my boolean function and changed some variables

--- Smith_Carl_Assignment2.js
+++ Smith_Carl_Assignment2.js
@@ -7,11 +7,9 @@ Javascript flowchart program
 
 //Variables
 var dead = "Jingo has lost all HP and died, congratulations on your victory!" //String Variable
-	enemyHP = 115 //Number Variable
-	boolean = 'true'
 	int = 13 //Number Variable
 	str = 10 //Number Variable
-	damage = int + str
+	maxdamage = int + str //Maximum damage that can be dealt in a single attack
 //prompt
 
 var name = prompt("What is your name, Hero?", "")
@@ -21,13 +19,37 @@ var name = prompt("What is your name, Hero?", "")
 
 //procedure
 
-var PrimaryElement = prompt("What is your primary element? Enter fire or earth")
+
 var PrimaryWeapon = prompt("What is your primary weapon?")
-
-
-if(PrimaryElement == 'fire') {
-	console.log("You are wielding the mythical Incendiary ", PrimaryWeapon, " of Disintigration!")
+var PrimaryElement = prompt("What is your primary element? Enter fire or earth")
+	if(PrimaryElement == 'fire') {
+		console.log("You are wielding the mythical Incendiary ", PrimaryWeapon, " of Disintigration!")
 }
-else {
-	console.log("You are wielding the mythical Tremmering ", PrimaryWeapon, " of Obliteration!")
+	else {
+		console.log("You are wielding the mythical Tremmering ", PrimaryWeapon, " of Obliteration!")
 }
+
+//Confirm
+
+var boolean = confirm("Are you ready to go on your first adventure and look for a battle? Click OK for yes.")
+
+//Boolean Function
+var Jingo = "While adventuring you find Jingo, a notorious assassin from the land of Draco Falls! Do you need to think of a strategy before you attack him? Click OK for yes."
+
+var prepare = confirm(Jingo)
+	if (prepare == true){
+		var ready = true
+	}
+	else {
+		var ready = false
+	}
+
+var prepandattacktime = 10
+
+console.log("While adventuring you find Jingo, a notorious assassin from the land of Draco Falls! Do you need to think of a strategy before you attack him?")
+
+while (prepandattacktime > 0){
+	console.log("Attack will start in " + prepandattacktime+"!")
+	prepandattacktime--
+}
+console.log("You have finished preparing and are now ready for the attack!")
